51a1f9542e659f1b948069b549e1d3421a7f78cc
Patricia Grings <patriciagrings@hotmail.com>
2024-07-08 16:24:20 -0300

    desenvolvimento

---
+++ AV1.js
@@ -0,0 +1,55 @@
+
+function tinta() {
+    var ttinta = document.getElementById('txtTinta')
+    var escolha = Number(ttinta.value)
+    switch (escolha) {
+        case 1:
+           var quantidade= 8
+        break
+        case 2:
+           var quantidade= 8
+        break
+        case 3:
+           var quantidade= 12
+        break
+        case 4:
+           var quantidade= 8
+        break
+        case 5:
+           var quantidade= 10
+        break
+        case 6:
+           var quantidade= 12
+        break
+        case 7:
+           var quantidade= 14
+        break
+        case 8:
+           var quantidade= 9
+        break
+    }
+}
+function paredes() {
+    var qtPar = document.getElementById('nParede')
+    var qtdPar = Number(qtPar.value)
+    var altPar = document.getElementById('hParede')
+    var hPar = Number(altPar.value)
+    var largPar = document.getElementById('lParede')
+    var lPar = Number(largPar.value)  
+    if (abert[0].checked) {
+        
+    }
+}
+    function aberturas() {
+        var qtAbert = document.getElementById('nAberturas')
+        var qtdAbert = Number(qtAbert.value)
+        var altAbert = document.getElementById('hAberturas')
+        var hAbert = Number(altAbert.value)
+        var largAbert = document.getElementById('lAberturas')
+        var lAbert = Number(largAbert.value)
+}    
+
+function calculoParede() {
+    for (var c=1; c=qtdPar; c++)
+        
+}
