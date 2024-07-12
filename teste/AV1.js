7f2fd4744a646d4c8bc8a719238c4d0790c761b1
Patricia Grings <patriciagrings@hotmail.com>
2024-07-11 21:02:47 -0300

    atualizações

--- teste/AV1.js
+++ teste/AV1.js
@@ -1,4 +1,4 @@
-var aParede = 0
+/*var aParede = 0*/
 var tipoTinta = 0
 var textoTinta= ''
 var aAberturas= 0
@@ -6,53 +6,49 @@ var aParedeTot=0
 var aAberturasTot=0
 var vetParedes = []
 var vetAberturas = []
+var vetTeste = []
+var elementoClicado=0
+var textoElemento=0
+var clique= evList('op1',tinta)
+var clique= evList('op2',tinta)
+var clique= evList('op3',tinta)
+var clique= evList('op4',tinta)
+var clique= evList('op5',tinta)
+var clique= evList('op6',tinta)
+var clique= evList('op7',tinta)
+var clique= evList('op8',tinta)
 
-function tinta() {
-    var ttinta = document.getElementById('txtTinta')    /*Pega do HTML o número da tinta escolhida*/
-    var escolha = Number(ttinta.value)  /*Converte para número a variável de escolha do tipo de tinta */
-    
-    switch (escolha) {  /*Seleção do tipo de tinta*/
-        case 1:
-            tipoTinta= 8
-            textoTinta= 'Látex ou PVA'
-        break
-        case 2:
-            tipoTinta= 8
-            textoTinta='Acrílica'
-        break
-        case 3:
-            tipoTinta= 12
-            textoTinta='Esmalte'
-        break
-        case 4:
-            tipoTinta= 8
-            textoTinta='Epóxi'
-        break
-        case 5:
-            tipoTinta= 10
-            textoTinta='Óleo'
-        break
-        case 6:
-            tipoTinta= 12
-            textoTinta='Verniz'
-        break
-        case 7:
-            tipoTinta= 14
-            textoTinta='Acrílica fosca premium'
-        break
-        case 8:
-            tipoTinta= 9
-            textoTinta='Epóxi à base de água'
-        break
+function evList(n1,n2) {
+    var clicar = document.getElementById(n1).addEventListener("click",n2)
+    return clicar
+}
+
+function tinta(){
+    elementoClicado = this
+    textoElemento = elementoClicado.value
+    if (elementoClicado.id === 'op1' || elementoClicado.id === 'op2' || elementoClicado.id === 'op4') {
+        textoElemento = 8;
+    }
+    else if (elementoClicado.id === 'op3' || elementoClicado.id === 'op6') {
+        textoElemento = 12;
+    }
+    else if (elementoClicado.id === 'op5') {
+        textoElemento = 10;
+    }
+    else if (elementoClicado.id === 'op7') {
+        textoElemento = 14;
+    }
+    else if (elementoClicado.id === 'op8') {
+        textoElemento = 9;
     }
 
     var tintas= document.getElementById('txtTipotinta') /*Cria uma variável para interagir com a div que vai mostrar o tipo de tinta escolhido*/
-    tintas.innerHTML= 'Você selecionou o tipo ' + escolha + ': ' + textoTinta
-    document.getElementById('txtTinta').value = "" /*Exibe no HTML o tipo de tinta escolhido */
+    tintas.innerHTML= 'Você selecionou tinta: ' + elementoClicado.innerText
+    document.getElementById('txtTinta').value = "" /*Limpa no HTML o tipo de tinta escolhido */
 }
 
 function paredes() {
-    aParede = 0
+    /*aParede = 0*/
     var altParede = document.getElementById('hParede').value    /* Pega a altura da parede do HTML */
     var altParede = parseFloat(altParede.replace(',', '.'))     /* Transforma a altura em n. real */
     var largParede = document.getElementById('lParede').value   /* Pega a largura da parede do HTML */
@@ -64,22 +60,26 @@ function paredes() {
         return
     }
     else{    
-    aParede= altParede*largParede   /* Calculo da área da parede */
+    aParede= calc(altParede,largParede)   /* Calculo da área da parede */
     vetParedes.push(aParede)        /* Coloca a área da parede no vetor */
     document.getElementById('hParede').value = ""   /* Limpa a entrada de altura no HTML */
     document.getElementById('lParede').value = ""   /* Limpa a entrada de largura no HTML */
     var parede= document.getElementById('txtParede')    /*Cria uma variável para interagir com a div que vai mostrar a área das paredes*/
     parede.innerHTML+= '<br>Área da parede: ' + aParede.toFixed(2)  /*Exibe no HTML a área das paredes */
-    
     }
+}
+
+function calc(n1,n2){
+    var multi= n1*n2
+    return multi
 }
 
 function aberturas(){
     aAberturas= 0
-    var altAbert = document.getElementById('hAberturas').value  /* Pega a altura da abertura do HTML */
-    var altAbert= parseFloat(altAbert.replace(',', '.'))    /* Transforma a altura em n. real */
-    var largAbert = document.getElementById('lAberturas').value /* Pega a largura da abertura do HTML */
-    var largAbert= parseFloat(largAbert.replace(',', '.'))  /* Transforma a largura em n. real */
+    var altAbert = document.getElementById('hAberturas').value    /* Pega a altura da parede do HTML */
+    var altAbert = parseFloat(altAbert.replace(',', '.'))     /* Transforma a altura em n. real */
+    var largAbert = document.getElementById('lAberturas').value   /* Pega a largura da parede do HTML */
+    var largAbert = parseFloat(largAbert.replace(',', '.'))   /* Transforma a largura em n. real */
     if (isNaN(altAbert) || isNaN(largAbert)) { /*Validação para obrigar que se insira valores antes de pressionar ok */
         window.alert('Preencha os campos de altura e largura corretamente!')
         document.getElementById('hAberturas').value = ""
@@ -87,7 +87,7 @@ function aberturas(){
         return
     }
     else{    
-        aAberturas= altAbert*largAbert  /* Calculo da área da abertura */
+        aAberturas= calc(altAbert,largAbert)  /* Calculo da área da abertura */
         vetAberturas.push(aAberturas)   /* Coloca a área da abertura no vetor */            
         document.getElementById('hAberturas').value = ""    /* Limpa a entrada de altura no HTML */
         document.getElementById('lAberturas').value = ""    /* Limpa a entrada de largura no HTML */
@@ -110,10 +110,10 @@ function calcular() {
         
     }
     var aTot= aParedeTot-aAberturasTot   /*Calcula a área útil de parede */
-    var calculo= aTot/tipoTinta /*Calcula a quantidade de tinta */
+    var calculo= aTot/textoElemento /*Calcula a quantidade de tinta */
     var calculoFinal = document.getElementById('txtCalculo2')   /*Cria uma variável para interagir com a div que vai mostrar a quantidade de tinta*/
-    calculoFinal.innerHTML = ''    /*Limpa no HTML a quantidade de tinta */
-    calculoFinal.innerHTML = 'O tipo de tinta utilizado foi: ' + textoTinta + '<br>A área total de paredes é de: ' + (aParedeTot.toFixed(2)) + 'm² <br>'+ 'A área total de aberturas é de: ' + (aAberturasTot.toFixed(2)) + 'm² <br>' + 'A área útil de paredes considerada para o cálculo é de: ' + (aTot.toFixed(2)) + 'm² <br>' + 'Devem ser utilizados ' + (calculo.toFixed(2)) + ' litros de tinta por demão.<br>No caso de duas demãos, deverão ser utilizados ' + ((calculo * 2).toFixed(2)) + ' litros de tinta.'    /*Exibe no HTML a quantidade de tinta */
+    calculoFinal.innerHTML = ''    /*Limpa no HTML a quantidade de tinta */ 
+    calculoFinal.innerHTML = 'O tipo de tinta utilizado foi: ' + elementoClicado.innerText + '<br>A área total de paredes é de: ' + (aParedeTot.toFixed(2)) + 'm² <br>'+ 'A área total de aberturas é de: ' + (aAberturasTot.toFixed(2)) + 'm² <br>' + 'A área útil de paredes considerada para o cálculo é de: ' + (aTot.toFixed(2)) + 'm² <br>' + 'Devem ser utilizados ' + (calculo.toFixed(2)) + ' litros de tinta por demão.<br>No caso de duas demãos, deverão ser utilizados ' + ((calculo * 2).toFixed(2)) + ' litros de tinta.'    /*Exibe no HTML a quantidade de tinta */
 } 
 function resetForm() {  /*Zera todas as variáveis e todos os campos para recomeçar o cálculo */
     aParede = 0
